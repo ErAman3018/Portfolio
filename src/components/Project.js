b96c665f4596cf10a236cec7f8f9dae92025d5a9
Aman <20001003018aman@dcrustm.org>
2023-11-24 17:43:42 +0530

    live link of latest work

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,6 +9,7 @@ const Project = ({item}) => {
       </div>
       <p className='capitalize text-accent text-sm mb-3'>{item.category}</p>
       <h3 className='text-2xl font-semibold capitalize mb-3'>{item.name}</h3>
+      {item.href && <a href={item.href}>Live link</a>}
     </div>
   )
 }
